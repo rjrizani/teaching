d0f7e99cf1dec6e951e4cd2d57568abcb031f7ee
rizani <45587977+rjrizani@users.noreply.github.com>
2025-03-08 07:08:50 +0000

    belajar variable

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("Hello Ahmad")
 print("Hi, how are you?", end=" ")
 print("this is my first python program")
+
+#valid indentifiers
+Myfile = "python"
+Date9_90 = "submit date"
+_ds = "hello"
+
+print(Myfile)
+print(Date9_90)
+print(_ds)
+
+"""
+#invalid indentifier
+data-rec = "pyhton"
+print(data-rec)
+"""
